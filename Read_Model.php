a7613ea4d2e685e79c55e091c23daa17d760abe3
Merajit <merajahmadit@gmail.com>
2020-02-29 04:41:54 +0530

    Create Read_Model.php

---
+++ Read_Model.php
@@ -0,0 +1,104 @@
+<?php 
+defined('BASEPATH') OR exit('No direct script access allowed'); 
+class Read_Model extends CI_Model{
+	
+	public function __construct() {
+		parent::__construct(); 
+	}
+	
+	public function getData($rowno,$rowperpage,$search="",$SubmitedDate="") 
+	{	
+		$this->db->select('*');
+		$this->db->from('tododiary');
+		
+		if($search != ''){
+			$this->db->like('toDo', $search);
+		}
+		
+		if($SubmitedDate != ''){
+			$this->db->like('SubmitedDate', $SubmitedDate);
+		}
+		
+		$this->db->limit($rowperpage, $rowno); 
+		$this->db->order_by('PostingDate', 'DESC');
+		$query = $this->db->get();
+		
+		return $query->result_array();  
+	}
+	
+	// Select total records
+	function getrecordCount($search = '') 
+	{
+		$this->db->select('count(*) as allcount');
+		$this->db->from('tododiary');
+
+		if($search != ''){
+			$this->db->like('toDo', $search);
+		}
+
+		$query = $this->db->get();
+		$result = $query->result_array();
+
+		return $result[0]['allcount'];
+	}
+
+	function getDiaryDetail($id)
+	{
+		$ret=$this->db->select('id,toDo,SubmitedDate,PostingDate')->where('id',$id)->get('tododiary');
+		return $ret->row();    
+	}
+	
+	function insertToDo($toDo,$SubmitedDate)
+	{
+		$data=array(
+			'toDo'=>$toDo,
+			'SubmitedDate'=>$SubmitedDate
+		);
+		$sql_query=$this->db->insert('tododiary',$data);
+		if($sql_query){
+			$this->session->set_flashdata('success', 'To do inserted successful');
+			redirect('read');
+		}
+		else{
+			$this->session->set_flashdata('error', 'Somthing went worng. Error!!');
+			redirect('read');
+		}
+	}
+	
+	function updateToDo($toDo,$SubmitedDate,$toDoid)
+	{
+		$data=array(
+			'toDo'=>$toDo,
+			'SubmitedDate'=>$SubmitedDate
+		);
+
+		$sql_query=$this->db->where('id', $toDoid)->update('tododiary', $data); 
+		if($sql_query){
+			$this->session->set_flashdata('success', 'To do updated successful');
+			redirect('read');
+		}
+		else{
+			$this->session->set_flashdata('error', 'Somthing went worng while Updating. Error!!');
+			redirect('read');
+		}
+
+	}
+	
+	function deleteToDo($toDoid)
+	{
+		$sql_query=$this->db->where('id', $toDoid)->delete('tododiary'); 
+		if($sql_query){
+			$this->session->set_flashdata('success', 'To do deleted successfully');
+			redirect('read');
+		}
+		else{
+			$this->session->set_flashdata('error', 'Somthing went worng. Error!!');
+			redirect('read');
+		}
+
+	}
+
+
+}
+
+
